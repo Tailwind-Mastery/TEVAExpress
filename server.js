b371c7d4da83efcbe5fb946791c487acf8836704
Tailwind Mastery <tailwindmastery@gmail.com>
2023-05-21 18:39:29 +0500

    a

--- server.js
+++ server.js
@@ -19,7 +19,10 @@ app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())  
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: [
+        'http://localhost:5173',
+        
+    ],
 }))
 
 app.use('/api/user', userRoutes)
