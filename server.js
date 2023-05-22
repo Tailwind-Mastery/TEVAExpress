5a2a05f29e52f4571f7ed1e2329293cc2016af96
Tailwind Mastery <tailwindmastery@gmail.com>
2023-05-22 12:18:57 +0500

    deploy

--- server.js
+++ server.js
@@ -19,8 +19,10 @@ app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())  
 
 // app.use(cors({origin: 'http://localhost:5173'}))
-app.use(cors({origin: '*'}))
-// app.use(cors({origin: 'https://tailwind-mastery.github.io/TEVAReact'}))
+// app.use(cors({origin: '*'}))
+app.use(cors({origin: [
+    'https://tailwind-mastery.github.io/TEVAReact'
+]}))
 
 app.use('/api/user', userRoutes)
 app.use('/api/product', productRoutes)
